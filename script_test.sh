551860692e807e9fbefca25b251a7b92d3fe2cc3
Eloi Perdereau <eloi@perdereau.eu>
2013-04-10 15:15:37 +0200

    update test script: print ok only if ok

--- script_test.sh
+++ script_test.sh
@@ -4,11 +4,12 @@
 # script de test pour le projet de compilation
 #
 
+ok=1
 for f in $(ls $1/*.pas)
 do
     echo "compil $f"
     if ! ./compil.run $f $2
-    then break;
-    fi
+    then ok=0; break; fi
 done
-echo "OK"
+if [ "$ok" -eq "1" ]
+then echo "OK"; fi
